5cfd900698bf174082210335ba13f60a0cd814dc
User <batman45@yandex.ru>
2023-02-27 23:06:22 +0300

    сформирован запрос для получения инфы

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = "60925936ffb2aea192fc36034702edba"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = 'Petrozavodsk'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request, 'weather/index.html')
