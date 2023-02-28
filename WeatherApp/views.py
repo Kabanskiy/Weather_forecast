530ad65a993f433accd04742188b55801fa48019
User <batman45@yandex.ru>
2023-02-28 22:25:07 +0300

    несены изменения для вывода инфы через поисковик

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 import requests
+from . models import City
 
 def index(request):
     appid = "60925936ffb2aea192fc36034702edba"
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-    city = 'Petrozavodsk'
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon'],
-    }
-    context = {'info': city_info}
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities}
     return render(request, 'weather/index.html', context)
